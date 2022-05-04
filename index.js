0661681293f93ef885d0d6c56ec79641a3f0615f
shatfield4 <seanhatfield5@gmail.com>
2022-05-04 01:24:51 -0700

    Initial commit

--- index.js
+++ index.js
@@ -23,23 +23,8 @@ app.use('/images', express.static('./storage/images'));
 
 app.get('/api/token/:token_id', function(req, res) {
   const tokenId = parseInt(req.params.token_id).toString()
-  // const person = db[tokenId]
-  // const bdayParts = person.birthday.split(' ')
-  // const day = parseInt(bdayParts[1])
-  // const month = parseInt(bdayParts[0])
-  // const data = {
-  //   'name': person.name,
-  //   'attributes': {
-  //     'birthday': person.birthday,
-  //     'birth month': monthName(month),
-  //     'zodiac sign': zodiac(day, month),
-  //     // 'age': moment().diff(person.birthday, 'years')
-  //   },
-  //   'image': `${HOST}/images/${tokenId}.png`
-  // }
 
   if (parseInt(tokenId) < 0 || parseInt(tokenId) > 8887) {
-    console.log('Token ID does not exist');
     errorData = {
       'Error' : 'Token ID does not exist'
     }
@@ -51,7 +36,6 @@ app.get('/api/token/:token_id', function(req, res) {
   fs.readFile('./storage/metadata/' + tokenId + '.json', (err, fileObj) => {
     if (err) throw err;
     let file = JSON.parse(fileObj);
-    // console.log(file['attributes']);
 
     const data = {
       "name": `Panda Paradise #${tokenId}`,
@@ -61,8 +45,6 @@ app.get('/api/token/:token_id', function(req, res) {
     }
     res.send(data)
 
-
-    // file['attributes'];
   });
 
 
@@ -72,28 +54,3 @@ app.get('/api/token/:token_id', function(req, res) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 })
-
-// returns the zodiac sign according to day and month ( https://coursesweb.net/javascript/zodiac-signs_cs )
-function zodiac(day, month) {
-  var zodiac =['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
-  var last_day =['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
-  return (day > last_day[month]) ? zodiac[month*1 + 1] : zodiac[month];
-}
-
-function monthName(month) {
-  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-  ]
-  return monthNames[month - 1]
-}
-
-function getAttributes(tokenId) {
-  let returnData = [];
-  fs.readFile('./storage/metadata/' + tokenId + '.json', (err, data) => {
-    if (err) throw err;
-    let file = JSON.parse(data);
-    console.log(file['attributes']);
-    returnData = file['attributes'];
-  });
-
-  return returnData;
-}
